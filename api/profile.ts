b6237ed5de1851b1e06dcdde6e47c2e67a7c89de
dragonfly161518 <por.nattapat.01@gmail.com>
2020-08-20 18:08:55 +0700

    change dir path

--- api/profile.ts
+++ api/profile.ts
@@ -8,7 +8,9 @@ export default async function (req: NowRequest, res: NowResponse) {
     // const result = await renderImage(String(req.query.name));
 
     res.setHeader("Content-Type", "image/svg+xml");
-    const image = createReadStream(join(__dirname, "assets", "giphy.gif"));
+    const image = createReadStream(
+      join(__dirname, "..", "assets", "giphy.gif")
+    );
     image.pipe(res);
   } catch (error) {
     res.send("Error");
