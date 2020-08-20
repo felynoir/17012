1c19a29a445d7bea1b0896b8874d624da3cb090a
dragonfly161518 <por.nattapat.01@gmail.com>
2020-08-20 17:57:03 +0700

    using files at runtime

--- api/profile.ts
+++ api/profile.ts
@@ -1,13 +1,14 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 // import { renderImage } from "../images";
-import fs from "fs";
+import { createReadStream } from "fs";
+const { join } = require("path");
 
 export default async function (req: NowRequest, res: NowResponse) {
   try {
     // const result = await renderImage(String(req.query.name));
 
     res.setHeader("Content-Type", "image/svg+xml");
-    const image = fs.createReadStream("/giphy.gif");
+    const image = createReadStream(join(__dirname, "assets", "giphy.gif"));
     image.pipe(res);
   } catch (error) {
     res.send("Error");
