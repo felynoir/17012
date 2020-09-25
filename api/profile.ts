02ad8c9a39b5dfd10cc46a1d19afa5b2484541f1
Nattapat Sriwichailumpun <por.nattapat.01@gmail.com>
2020-09-25 18:10:22 +0700

    rendered Image

--- api/profile.ts
+++ api/profile.ts
@@ -5,13 +5,10 @@ const { join } = require("path");
 
 export default async function (req: NowRequest, res: NowResponse) {
   try {
+    res.setHeader("Content-Type", "image/svg+xml");
     const result = await renderImage(String(req.query.name));
 
-    res.setHeader("Content-Type", "image/svg+xml");
-    const image = createReadStream(
-      join(__dirname, "..", "assets", "giphy.gif")
-    );
-    image.pipe(res);
+    res.send(result)
   } catch (error) {
     res.send("Error");
     console.error(error);
